2ccdd2432df9cb17920f5e6a9f064d2c2de82340
abramwf <kemocengbersih@gmail.com>
2023-05-29 23:06:51 +0700

    tambahkan folder css admin

--- resources/views/admin/adminHome.blade.php
+++ resources/views/admin/adminHome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/adminHome.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin/adminHome.css') }}">
     <link rel="stylesheet" href="{{ asset('css/nav.css') }}">
     <link rel="stylesheet" href="{{ asset('css/env.css') }}">
     <title>Admin Home</title>
--- resources/views/admin/adminView.blade.php
+++ resources/views/admin/adminView.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('css/adminView.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/admin/adminView.css') }}">
     <link rel="stylesheet" href="{{ asset('css/nav.css') }}">
     <link rel="stylesheet" href="{{ asset('css/env.css') }}">
     <title>Admin List Page Item</title>
